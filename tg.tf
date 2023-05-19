b82808f01d7d62e91062c9489fd1f339e8ddb3a0
unknown <anilkumarravuri510@gmail.com>
2023-05-19 15:20:09 +0530

    auto push

--- tg.tf
+++ tg.tf
@@ -15,18 +15,18 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 }
 
 // ADD A RULE INSIDE THE LISTENER
-resource "aws_lb_listener_rule" "app_rule" {
-  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+# resource "aws_lb_listener_rule" "app_rule" {
+#   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+#   priority     = 100
 
-  action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
+#   action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
+#     }
+#   }
+# }
