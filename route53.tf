fa9c9f26463a9a32f6de203ebb9bef52c9d4a22c
unknown <anilkumarravuri510@gmail.com>
2023-05-19 15:58:13 +0530

    auto push

--- route53.tf
+++ route53.tf
@@ -1,8 +1,8 @@
 # THIS CREATES THE C NAME RECORD FOR COMPONENTS
 resource "aws_route53_record" "record" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+  zone_id =var.LB_TYPE == "internal" ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
   name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "CNAME"
   ttl     = 10
-  records = [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS]
+  records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
--- vars.tf
+++ vars.tf
@@ -5,3 +5,4 @@ variable "OD_INSTANCE_COUNT" {}
 variable "COMPONENT" {}
 variable "APP_PORT" {}
 variable "APP_VERSION" {}
+//variable "LB_TYPE" {}
