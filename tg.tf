970ed6c19563f778ebe7529d6157518410693dac
unknown <anilkumarravuri510@gmail.com>
2023-05-21 00:00:14 +0530

    auto push

--- tg.tf
+++ tg.tf
@@ -18,7 +18,7 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 resource "aws_lb_listener_rule" "app_rule" {
   count        =  var.LB_TYPE == "internal" ? 1 : 0
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = random_integer.priority.result
 
   action {
     type             = "forward"
@@ -38,17 +38,17 @@ resource "random_integer" "priority" {
   max = 500
  }
 
-# # PUBLIC ALB LISTENER  
-# # CREATES A LISTENER TO THE TARGET GROUP
-# resource "aws_lb_listener" "public" {
-#   count            =  var.LB_TYPE == "internal" ? 0 : 1
-#   load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-#   port              = "80"
-#   protocol          = "HTTP"
+# PUBLIC ALB LISTENER  
+# CREATES A LISTENER TO THE TARGET GROUP
+resource "aws_lb_listener" "public" {
+  count            =  var.LB_TYPE == "internal" ? 0 : 1
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.app.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
   
