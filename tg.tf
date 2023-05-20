3db10fc74af0b8e82170ac9e5f384a04f4434621
unknown <anilkumarravuri510@gmail.com>
2023-05-20 23:38:15 +0530

    auto push

--- tg.tf
+++ tg.tf
@@ -16,7 +16,7 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 
 // ADD A RULE INSIDE THE LISTENER (PRIVATE)
 resource "aws_lb_listener_rule" "app_rule" {
- // count        =  var.LB_TYPE == "internal" ? 1 : 0
+  count        =  var.LB_TYPE == "internal" ? 1 : 0
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = 100
 
@@ -31,3 +31,24 @@ resource "aws_lb_listener_rule" "app_rule" {
     }
   }
 }
+
+# GENERATES A RANDOM INTEGAR
+resource "random_integer" "priority" {
+  min = 101
+  max = 500
+ }
+
+# PUBLIC ALB LISTENER  
+# CREATES A LISTENER TO THE TARGET GROUP
+resource "aws_lb_listener" "public" {
+  count = var.INTERNAL ? 1 : 0
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
+  
