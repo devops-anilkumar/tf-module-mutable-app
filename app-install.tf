a4cdd7146a2af2c3f2b78f5dc0794fa1298e89c2
unknown <anilkumarravuri510@gmail.com>
2023-05-21 00:19:08 +0530

    auto push

--- app-install.tf
+++ app-install.tf
@@ -1,6 +1,8 @@
 resource "null_resource" "app" {
  //  triggers = {var = var.APP_VERSION}   # WHENEVER THERE IS A CHANGE IN THE APP VERSION IT TRIGGERS
-triggers = { always = true }    // this ensures it will run all the time
+ //triggers = { always = true }   
+  triggers     = {  timestamp =    timestamp() }  // This ENSURES IT will run all the time
+
 count        = var.SPOT_INSTANCE_COUNT + var.OD_INSTANCE_COUNT
 
  #DECLEARING THE REMOTE PROVISIONER INSIDE THE RESOURCE 
