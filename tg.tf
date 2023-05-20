5e702417fa86ed0a6a4f08136d29d3ea882aa8e1
unknown <anilkumarravuri510@gmail.com>
2023-05-20 23:54:10 +0530

    auto push

--- tg.tf
+++ tg.tf
@@ -38,17 +38,17 @@ resource "random_integer" "priority" {
   max = 500
  }
 
-# PUBLIC ALB LISTENER  
-# CREATES A LISTENER TO THE TARGET GROUP
-resource "aws_lb_listener" "public" {
-  count        =  var.LB_TYPE == "internal" ? 0 : 1
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-  port              = "80"
-  protocol          = "HTTP"
+# # PUBLIC ALB LISTENER  
+# # CREATES A LISTENER TO THE TARGET GROUP
+# resource "aws_lb_listener" "public" {
+#   count            =  var.LB_TYPE == "internal" ? 0 : 1
+#   load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+#   port              = "80"
+#   protocol          = "HTTP"
 
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.app.arn
+#   }
+# }
   
