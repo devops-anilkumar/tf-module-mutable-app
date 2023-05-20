02fa6514e122f9426c73856a34251d3a4479933e
unknown <anilkumarravuri510@gmail.com>
2023-05-20 22:46:59 +0530

    auto push

--- tg.tf
+++ tg.tf
@@ -14,8 +14,9 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
   port               = 8080
 }
 
-// ADD A RULE INSIDE THE LISTENER
+// ADD A RULE INSIDE THE LISTENER (PRIVATE)
 resource "aws_lb_listener_rule" "app_rule" {
+ // count        =  var.LB_TYPE == "internal" ? 1 : 0
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = 100
 
