df1bba123e3f24fb3f1ac0827ad82fc66834c836
unknown <anilkumarravuri510@gmail.com>
2023-05-23 15:48:05 +0530

    auto push

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ resource "aws_spot_instance_request" "spot" {
   wait_for_fulfillment    = true
   vpc_security_group_ids  = [aws_security_group.allow_app.id]
   subnet_id               = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
-
+  iam_instance_profile    = "aws-admin"
   # tags = {
   #   Name = "${var.COMPONENT}-${var.ENV}-spot"
   # }
@@ -18,7 +18,8 @@ resource "aws_instance" "od" {
   ami                     = data.aws_ami.ami.id
   instance_type           = var.INSTANCE_TYPE
   vpc_security_group_ids  = [aws_security_group.allow_app.id]
-    subnet_id               = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  subnet_id               = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  iam_instance_profile    = "aws-admin"
 
   tags = {
     Name = "${var.COMPONENT}-${var.ENV}"  
